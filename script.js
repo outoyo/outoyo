2687d8f4e2349cb4caab2475feb0ac1c0e3f035a
Outoyo <outoyo.moko@gmail.com>
2024-10-25 17:33:20 +0300

    af

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Босня Александр Михайлович";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
